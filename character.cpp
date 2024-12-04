28c7a999df7b36e9dacd5da4acdf43ac3c2de278
Neha panbude <nehapanbude123@gmail.com>
2024-12-05 05:20:09 +0530

    first commit

---
+++ character.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n = 4;
+
+    for (int i=0; i<n; i++){//outer
+    char ch ='A';
+         
+         for(int j=0; j<n; j++){//inner start=>line start
+         cout << ch;
+         ch = ch + 1; //65 + 1=>66
+    }
+    cout << endl;
+}
+return 0;
+}
